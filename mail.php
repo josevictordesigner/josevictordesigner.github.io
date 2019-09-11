0b1abca98d4e0afced30deb91f856a336939b342
José Victor <jv.lopes@live.com>
2019-09-11 13:49:42 -0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "contato@terratempoaudiovisual.com.br";
+$recipient = "jv.lopes@live.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
